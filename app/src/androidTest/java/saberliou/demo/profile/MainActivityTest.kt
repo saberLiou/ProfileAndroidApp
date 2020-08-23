d6a94f407d4178883f71c9516a5ba5a09469777d
Guo-Xun Liu <saberliou@gmail.com>
2020-08-23 21:59:07 +0800

    Import Material Dialog and modify MainActivityTest and MainActivity UI to enable update developer's data temporarily.

--- app/src/androidTest/java/saberliou/demo/profile/MainActivityTest.kt
+++ app/src/androidTest/java/saberliou/demo/profile/MainActivityTest.kt
@@ -1,9 +1,11 @@
 package saberliou.demo.profile
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
@@ -20,9 +22,58 @@ class MainActivityTest {
     var mainActivityTestResult = ActivityTestRule(MainActivity::class.java)
 
     @Test
-    fun launchApp() {
+    fun activityLaunched() {
         onView(withId(R.id.ivDeveloperImage)).check(matches(withDrawable(R.drawable.head_photo)))
+
+        // Developer's name
         onView(withId(R.id.tvDeveloperName)).check(matches(withText(R.string.mainActivity_tvDeveloperName_text)))
+        onView(withId(R.id.ibtnDeveloperEditName)).check(matches(withDrawable(R.drawable.edit)))
+
+        // Developer's motto
         onView(withId(R.id.tvDeveloperMotto)).check(matches(withText(R.string.mainActivity_tvDeveloperMotto_text)))
+        onView(withId(R.id.ibtnDeveloperEditMotto)).check(matches(withDrawable(R.drawable.edit)))
+    }
+
+    @Test
+    fun useEditDialogsToUpdateDeveloperData() {
+        // Developer's name
+        val updatedName = "Guo-Xun Liu"
+        onView(withId(R.id.ibtnDeveloperEditName)).perform(click())
+        onView(withText(R.string.mainActivity_mdEditDeveloperNameTitle_text)).check(
+            matches(
+                isDisplayed()
+            )
+        )
+        onView(withText(R.string.mainActivity_mdEditDeveloperButton_text)).perform(click())
+        onView(withText(R.string.mainActivity_mdEditDeveloperNameTitle_text)).check(
+            matches(
+                isDisplayed()
+            )
+        )
+
+        onView(withId(R.id.md_input_message)).perform(typeText(updatedName))
+        onView(withText(R.string.mainActivity_mdEditDeveloperButton_text)).perform(click())
+        onView(withText(R.string.mainActivity_mdEditDeveloperNameTitle_text)).check(doesNotExist())
+        onView(withId(R.id.tvDeveloperName)).check(matches(withText(updatedName)))
+
+        // Developer's motto
+        val updatedMotto = "No codes, no life."
+        onView(withId(R.id.ibtnDeveloperEditMotto)).perform(click())
+        onView(withText(R.string.mainActivity_mdEditDeveloperMottoTitle_text)).check(
+            matches(
+                isDisplayed()
+            )
+        )
+        onView(withText(R.string.mainActivity_mdEditDeveloperButton_text)).perform(click())
+        onView(withText(R.string.mainActivity_mdEditDeveloperMottoTitle_text)).check(
+            matches(
+                isDisplayed()
+            )
+        )
+
+        onView(withId(R.id.md_input_message)).perform(typeText(updatedMotto))
+        onView(withText(R.string.mainActivity_mdEditDeveloperButton_text)).perform(click())
+        onView(withText(R.string.mainActivity_mdEditDeveloperMottoTitle_text)).check(doesNotExist())
+        onView(withId(R.id.tvDeveloperMotto)).check(matches(withText(updatedMotto)))
     }
 }
--- app/src/main/java/saberliou/demo/profile/MainActivity.kt
+++ app/src/main/java/saberliou/demo/profile/MainActivity.kt
@@ -1,11 +1,39 @@
 package saberliou.demo.profile
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.input.input
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ibtnDeveloperEditName.setOnClickListener {
+            launchMaterialDialog(
+                R.string.mainActivity_mdEditDeveloperNameTitle_text,
+                tvDeveloperName
+            )
+        }
+
+        ibtnDeveloperEditMotto.setOnClickListener {
+            launchMaterialDialog(
+                R.string.mainActivity_mdEditDeveloperMottoTitle_text,
+                tvDeveloperMotto
+            )
+        }
+    }
+
+    private fun launchMaterialDialog(titleTextResourceId: Int, updatedTextView: TextView) {
+        MaterialDialog(this).show {
+            input(waitForPositiveButton = true, allowEmpty = false) { _, value ->
+                updatedTextView.text = value
+            }
+            title(titleTextResourceId)
+            positiveButton(R.string.mainActivity_mdEditDeveloperButton_text)
+        }
     }
 }
