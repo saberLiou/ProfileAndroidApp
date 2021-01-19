16e17b906b8f4a969b7c8abaf0419ac3a78ee4d5
Guo-Xun Liu <saberliou@gmail.com>
2021-01-19 23:33:34 +0800

    Optimize UI of ContactMeFragment and Toolbar in MainActivity.

--- app/src/main/java/saberliou/demo/profile/contactme/ContactMeFragment.kt
+++ app/src/main/java/saberliou/demo/profile/contactme/ContactMeFragment.kt
@@ -4,20 +4,34 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import saberliou.demo.profile.R
+import saberliou.demo.profile.databinding.FragmentContactMeBinding
 
 @AndroidEntryPoint
 class ContactMeFragment : Fragment() {
+    private lateinit var binding: FragmentContactMeBinding
     private val viewModel: ContactMeViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_contact_me, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_contact_me, container, false)
+        binding.viewModel = viewModel
+        return binding.root
     }
+
+//    private fun enterToHideKeyBoard(view: View, keyCode: Int): Boolean {
+//        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+//            (requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
+//                .hideSoftInputFromWindow(view.windowToken, 0)
+//            return true
+//        }
+//        return false
+//    }
 }
